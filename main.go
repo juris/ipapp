82862614f87dee050c8c28e6b56cfa8faccd3f56
Juris <juris@zee.lv>
2020-04-29 20:02:42 +0300

    Switch from chi to httprouter

--- main.go
+++ main.go
@@ -6,22 +6,13 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
+	"github.com/gorilla/handlers"
+	"github.com/julienschmidt/httprouter"
 )
-
-var router *chi.Mux
-
-func init() {
-	router = chi.NewRouter()
-	router.Use(
-		middleware.Logger,
-		middleware.Recoverer,
-	)
-}
 
 func main() {
 
+	router := httprouter.New()
 	port, envExists := os.LookupEnv("IPAPP_PORT")
 	if envExists {
 		port = ":" + port
@@ -29,12 +20,13 @@ func main() {
 		port = ":80"
 	}
 
-	router.Get("/*", func(w http.ResponseWriter, r *http.Request) {
+	router.GET("/*any", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		ip, _, _ := net.SplitHostPort(r.RemoteAddr)
 		w.Write([]byte(ip + "\n"))
 	})
 
-        log.Printf("Starting app on %s", port)
-	log.Fatal(http.ListenAndServe(port, router))
+	log.Printf("Starting app on %s", port)
+	loggedRouter := handlers.CombinedLoggingHandler(os.Stdout, router)
+	log.Fatal(http.ListenAndServe(port, handlers.RecoveryHandler()(loggedRouter)))
 
 }
